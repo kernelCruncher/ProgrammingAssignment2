9ec504efa2a4d22b86125d486a973f65c65a3a64
Peter <judgee21@hotmail.co.uk>
2018-09-29 19:46:36 +0100

    Fixed typo.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function (makeCacheMatrix) outputs a list of useful functions (get, set, setMatrixInverse, getMatrixInverse) to be invoked by a subsequent function (cacheSolve).
-## Within makeCacheMatrix the inverse of the inputted matrix (x) is stored as the variable 'm' when it is externally calculated.
+## This function (makeCacheMatrix) outputs a list of useful functions (get, set, setMatrixInverse, getMatrixInverse) to be invoked 
+## by a subsequent function (cacheSolve). Within makeCacheMatrix the inverse of the inputted matrix (x) is stored as the variable 'm' 
+## when it is externally calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -22,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Here the real work is done. If the makeCahceInverse is inputted as a  parameter
+## Here the real work is done. If the makeCacheInverse is inputted as a  parameter
 ## in the cacheSolve function then cacheSolve looks to see if the variable m is set in
 ## makeCacheMatrix which corresponds to the inverse. If it is, the function returns
 ##this value. If not, the inverse is calculated in the cacheSolve function and stored
